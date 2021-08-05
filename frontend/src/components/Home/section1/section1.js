1f6d7009ea4b91ec1ac9d38c36904aec8b0d35ba
Gauri-g <gaurigupta2727@gmail.com>
2021-08-05 22:55:16 +0530

    fix: Redirects

--- frontend/src/components/Home/section1/section1.js
+++ frontend/src/components/Home/section1/section1.js
@@ -1,22 +1,24 @@
-import './section1.css'
-import img1 from "../../../images/img1.svg";
+import "./section1.css";
+import img1 from "../../../images/img1.svg"; 
 
+const Section1 = () => {
+  return (
+    <div className="row section1 " id="home">
+      <div className="col-lg-5">
+        <h1 className="extra-bold">Doc Diff</h1>
+        <h3 className="blue">By ISTE-VIT</h3>
+        <h3 className="txt">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </h3>
+        <button className="nav-link btn3" href="#otherresources">
+          Go to the Editor
+        </button>
+      </div>
+      <div className="col-lg-6">
+        <img src={img1} className="container img1" alt="timemator" />
+      </div>
+    </div>
+  );
+};
 
-const section1 = () => {
-    return(
-        <div className="row section1 " id="home">
-            <div className="col-lg-5">
-                <h1 className="extra-bold">Doc Diff</h1>
-                <h3 className="blue">By ISTE-VIT</h3>
-                <h3 className="txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-                <button className="nav-link btn3" href="#otherresources">Go to the Editor
-                </button>
-            </div>
-            <div className="col-lg-6">
-            <img src={img1} className="container img1" alt="timemator"/>
-            </div>
-        </div>
-    )
-}
-
-export default section1;
+export default Section1;
