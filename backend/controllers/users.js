b8cfedde6065af1a588f3d5fe8a970f3bc77b391
shashank-suresh <shashanksuresh3@gmail.com>
2021-08-20 12:48:08 +0530

    Updated User model and routes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,41 +1,11 @@
 const dotenv = require('dotenv').config();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = process.env.CLIENT_ID
-const client = new OAuth2Client(`${CLIENT_ID}`)
 
 const User = require('../models/User.js');
 
-const signIn = async (req, res) => {
-    const { email, password } = req.body
-    try {
-        const existingUser = await User.findOne({email})
-        if (!existingUser) return res.status(404).json({message: "User doesn't exist"})
-
-        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
-        if (!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"})
-
-        const token = jwt.sign({
-            id: existingUser._id,
-            email: existingUser.email
-        }, process.env.SECRET, {expiresIn: "1h"})
-
-        res.status(201).json({result: existingUser, token})
-    } catch (error) {
-        res.status(500).json({message: "Something went wrong"})
-    }
-}
-
 const signUp = async (req, res) => {
-    const { email, password, confirmPassword } = req.body
+    const { email, password } = req.body
+
     try {
-        const existingUser = await User.findOne({email})
-        if (existingUser) return res.status(404).json({message: "User already exists"})
-
-        if (password !== confirmPassword) return res.status(404).json({message: "Passwords do not match"})
-
         const hashedPassword = await bcrypt.hash(password, 12)
 
         const result = await User.create({
@@ -43,51 +13,27 @@ const signUp = async (req, res) => {
             password: hashedPassword
         })
 
-        const token = jwt.sign({
-            id: result._id,
-            email: result.email
-        }, process.env.SECRET, {expiresIn: "1h"})
-        res.status(201).json({message: "Successful signup"}) 
-
+        res.status(201).json({ message: "Successful signup" })
     } catch (error) {
-        res.status(500).json({message: "Something went wrong"})
+        res.status(500).json({ message: "Something went wrong" })
     }
 }
 
 const google = (req, res) => {
-    const { tokenId } = req.body;
+    const { email, tokenId } = req.body;
 
-    client.verifyIdToken({idToken: tokenId, audience: `${CLIENT_ID}`}).then(async (response) => {
-        const { email_verified, email, sub } = response.payload;
-
-        if (email_verified) {
-            const existingUser = await User.findOne({email})
-            if (existingUser) {
-                const token = jwt.sign({
-                    id: existingUser._id,
-                    email: existingUser.email
-                }, process.env.SECRET, {expiresIn: "1h"})
-        
-                return res.status(200).json({result: existingUser, token})
-            }
-
-            const hashedPassword = await bcrypt.hash(sub, 12)
-
-            const result = await User.create({
-                email,
-                password: hashedPassword
-            })
-
-            const token = jwt.sign({
-                id: result._id,
-                email: result.email
-            }, process.env.SECRET, {expiresIn: "1h"})
-
-            res.status(201).json({message: "Successful login"}) 
-        } else {
-            res.status(404).json({message: "Invalid credentials"})
-        }
-    })
+    try {
+        const hashedPassword = await bcrypt.hash(tokenId, 12)
+    
+        const result = await User.create({
+            email,
+            password: hashedPassword
+        })
+    
+        res.status(201).json({ message: "Successfully registered with Google" })
+    } catch (error) {
+        res.status(404).json({ message: "Invalid credentials" })
+    }
 }
 
-module.exports = {signIn, signUp, google};
+module.exports = { signUp, google };
