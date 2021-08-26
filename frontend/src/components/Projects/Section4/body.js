5ad024f6f213af3e803102376437e96b2f14e1d5
Gauri-g <gaurigupta2727@gmail.com>
2021-08-26 18:41:53 +0530

    fix: Porjects page

--- frontend/src/components/Projects/Section4/body.js
+++ frontend/src/components/Projects/Section4/body.js
@@ -1,55 +1,52 @@
 import "./section4.css";
 import cookie from "react-cookies";
-import { useState } from "react";
-import { Redirect } from "react-router";
-import img4 from "../../../images/img4.svg";
+import { useEffect, useState } from "react";  
+import img5 from "../../../images/22.jpg";
+ 
 
 const Body = () => {
   const uid = cookie.load("key");
-  const [projects, setProjects] = useState(false);
+  const [projects, setProjects] = useState(false); 
   const requestOptions = {
     method: "GET",
-  };
-  fetch(`http://localhost:5000/projects/all?uid=${uid}`, requestOptions)
-    .then((response) => {
-      const data = response.json();
-      return data;
-    })
-    .then((data) => {
-      setProjects(data);
+  }; 
+
+  useEffect(()=> {
+    fetch(`http://localhost:5000/projects/all?uid=${uid}`, requestOptions)
+  .then((response) => {   
+    console.log("abhi")
+    const data = response.json();
+    return data;
+  })
+  .then((data) => { 
+      console.log(data)
+      setProjects(data); 
     })
     .catch((error) => {
       console.log(error);
-    });
+    }); 
+  },[])
+  
+  
 
   const handleClick = (id) => {
-
     cookie.save("id", id, { path: "/" });   
-    window.location.href = "http://localhost:3000/editor"
-    // fetch(`http://localhost:5000/projects/${id}`, requestOptions)
-    //   .then((response) => {
-    //     const data = response.json();
-    //     return data;
-    //   })
-    //   .then((data) => {
-    //     console.log(data.files);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+    window.location.href = "http://localhost:3000/editor" 
   };
-
+ 
+ 
   return (
     <div className="projects">
       <h1 className="blue sec2head extra-bold">Projects</h1>
       <div className="row cards">
         {projects &&
           projects.map((project) => {
-            let id = project._id
+            let id = project._id  
+            console.log(project.name)
             return (
               <div className="col-lg-4">
                 <div className="card" onClick={() => {handleClick(id)}}>
-                  <img src={img4} className="card-img" alt="timemator" />
+                  <img src={img5} className="card-img" alt="timemator" />
                   <div className="card-body">
                     <h5 className="card-title">{project.name}</h5>
                   </div>
