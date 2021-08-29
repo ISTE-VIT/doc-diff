d25e2a87eb3044dff22b6086d7fcfde062d00c39
shashank-suresh <shashanksuresh3@gmail.com>
2021-08-29 13:44:44 +0530

    Implemented delete functionality

--- frontend/src/components/Projects/Section4/DeleteHandler.js
+++ frontend/src/components/Projects/Section4/DeleteHandler.js
@@ -5,10 +5,7 @@ import axios from "axios";
 const DeleteHandler = (id) => {
     let body; 
     const uid = cookie.load("key");
-    body = {uid,id}
-    axios.delete("http://localhost:5000/projects/delete", body).then((res) => {
-        console.log(res);
-    });
+    axios.delete(`http://localhost:5000/projects/delete?id=${id}&uid=${uid}`)
 }
 
 export default DeleteHandler
